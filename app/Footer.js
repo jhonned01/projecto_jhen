657188d86898413049d938eb7e3c4a4833bb623d
jhonned01 <jhonned01@gmail.com>
2023-05-17 18:25:45 -0500

    changes

--- app/Footer.js
+++ app/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <>
-      <hr className="my-6 border-blueGray-300" />
+      <hr className="my-4 border-blueGray-300" />
       <div className="flex flex-wrap items-center md:justify-between justify-center">
-        <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+        <div className="w-full md:w-4/12 p-4  mx-auto text-center">
           <div className="text-sm text-blueGray-500 font-semibold py-1">
             Copyright © <span id="get-current-year">2024</span>
             <a
