235eca1311bfeab0839cc4876735b030e864570c
jhonned01 <jhonned01@gmail.com>
2023-05-18 00:22:48 -0500

    ultimos cambios jhen

--- app/page.js
+++ app/page.js
@@ -8,10 +8,9 @@ export default function HomePage() {
   const [data, setData] = useState([
     {
       id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      price: 109.95,
-      description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      title: "Buzo Manga Larga ",
+      price: 55.9,
+      description: "Buzo streewear de algodon,con un diseño diferente ",
       category: "men's clothing",
       image: "/img1.jpeg",
       rating: {
@@ -21,11 +20,9 @@ export default function HomePage() {
     },
     {
       id: 2,
-      title: "Mens Casual Premium Slim Fit T-Shirts ",
-      price: 22.3,
-      description:
-        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      category: "men's clothing",
+      title: "Pantalon de Bota Tubo,Marfil ",
+      price: 75.9,
+      description: "Pantalon de color solido,con mucho estilo",
       image: "/img2.jpeg",
 
       rating: {
@@ -35,10 +32,9 @@ export default function HomePage() {
     },
     {
       id: 3,
-      title: "Mens Cotton Jacket",
-      price: 55.99,
-      description:
-        "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+      title: "Camiseta Manga Corta-Negra",
+      price: 29.9,
+      description: "Camisa Basica negra overzise",
       category: "men's clothing",
       image: "/img3.jpeg",
       rating: {
@@ -48,10 +44,9 @@ export default function HomePage() {
     },
     {
       id: 4,
-      title: "Mens Casual Slim Fit",
-      price: 15.99,
-      description:
-        "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+      title: "Camiseta Manga Corta Negro",
+      price: 33.9,
+      description: "Diseño En Estampado Con Estilo Animado  ",
       category: "men's clothing",
       image: "/img4.jpeg",
       rating: {
@@ -61,12 +56,10 @@ export default function HomePage() {
     },
     {
       id: 5,
-      title:
-        "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-      price: 695,
-      description:
-        "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-      category: "jewelery",
+      title: "Bermuda Tiro Medio Multicolor",
+      price: 35.0,
+      description: "Bermuda Comoda Multicolor",
+      category: "men's clothing",
       image: "/img5.jpeg",
       rating: {
         rate: 4.6,
@@ -75,11 +68,10 @@ export default function HomePage() {
     },
     {
       id: 6,
-      title: "Solid Gold Petite Micropave ",
-      price: 168,
-      description:
-        "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-      category: "jewelery",
+      title: "Camiseta Manga Corta Blanco ",
+      price: 35.9,
+      description: "Casimeta Con Diseño Colorido y Estampado de Letras",
+      category: "",
       image: "/img6.jpeg",
       rating: {
         rate: 3.9,
@@ -88,11 +80,10 @@ export default function HomePage() {
     },
     {
       id: 7,
-      title: "White Gold Plated Princess",
-      price: 9.99,
-      description:
-        "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
-      category: "jewelery",
+      title: "Camiseta Manga Corta",
+      price: 27.9,
+      description: "camiseta manga corta unisex multicolor",
+      category: "",
       image: "/img7.jpeg",
       rating: {
         rate: 3,
@@ -101,11 +92,10 @@ export default function HomePage() {
     },
     {
       id: 8,
-      title: "Pierced Owl Rose Gold Plated Stainless Steel Double",
-      price: 10.99,
-      description:
-        "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
-      category: "jewelery",
+      title: "Buzo",
+      price: 50.9,
+      description: "Buzo Manga larga con Patron de Manchas de Puma ",
+      category: "",
       image: "/img8.jpeg",
       rating: {
         rate: 1.9,
@@ -114,11 +104,10 @@ export default function HomePage() {
     },
     {
       id: 9,
-      title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
-      price: 64,
-      description:
-        "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
-      category: "electronics",
+      title: "buzo Manga Lagar Negro",
+      price: 45.0,
+      description: "buzo manfa larga negro con estampado sencillo ",
+      category: "",
       image: "/img9.jpeg",
       rating: {
         rate: 3.3,
@@ -127,11 +116,10 @@ export default function HomePage() {
     },
     {
       id: 10,
-      title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
-      price: 109,
-      description:
-        "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
-      category: "electronics",
+      title: "Vestido de Baño Entero",
+      price: 37.5,
+      description: "Vestido de Baño Entero Con Tonos Pasteles y Palmeras",
+      category: "",
       image: "/img10.jpeg",
       rating: {
         rate: 2.9,
@@ -140,12 +128,10 @@ export default function HomePage() {
     },
     {
       id: 11,
-      title:
-        "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
-      price: 109,
-      description:
-        "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
-      category: "electronics",
+      title: "Camiseta Manga Corta Negra",
+      price: 70.0,
+      description: "Camiseta Basica negro Manga Corta ",
+      category: "",
       image: "/img11.jpeg",
       rating: {
         rate: 4.8,
@@ -154,12 +140,10 @@ export default function HomePage() {
     },
     {
       id: 12,
-      title:
-        "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
-      price: 114,
-      description:
-        "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
-      category: "electronics",
+      title: "Camiseta Manga Corta Marfil",
+      price: 23.0,
+      description: "Camiseta Manga Corta Marfil  Basica Con Estampado Pequeño",
+      category: "",
       image: "/img12.jpeg",
       rating: {
         rate: 4.8,
@@ -168,11 +152,10 @@ export default function HomePage() {
     },
     {
       id: 13,
-      title: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
-      price: 599,
-      description:
-        "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
-      category: "electronics",
+      title: "Buzo manga larga multicolor",
+      price: 45,
+      description: "Buzo Manga Larga Multicolor Con Tela Suave",
+      category: "",
       image: "/img13.jpeg",
       rating: {
         rate: 2.9,
@@ -181,12 +164,10 @@ export default function HomePage() {
     },
     {
       id: 14,
-      title:
-        "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED ",
-      price: 999.99,
-      description:
-        "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
-      category: "electronics",
+      title: "Buzo manga larga multicolor ",
+      price: 47.9,
+      description: "Buzo Manga Larga Multicolor Con Tela Suave,Con Patron",
+      category: "",
       image: "/img14.jpeg",
       rating: {
         rate: 2.2,
@@ -195,11 +176,11 @@ export default function HomePage() {
     },
     {
       id: 15,
-      title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
-      price: 56.99,
+      title: "Buzo Manga Larga Multicolor ",
+      price: 35.9,
       description:
-        "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
-      category: "women's clothing",
+        "Buzo Manga Larga Multicolor Con Tela Suave,Con cuadros de color",
+      category: "",
       image: "/img15.jpeg",
       rating: {
         rate: 2.6,
